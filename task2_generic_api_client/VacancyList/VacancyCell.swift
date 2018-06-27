a0fad7a5aed7550dc48008b65401ee4044aed739
Roman <cold.steel.br@gmail.com>
2018-06-27 11:46:43 +0700

    In-memory search & UI binding implemented

---
+++ task2_generic_api_client/VacancyList/VacancyCell.swift
@@ -0,0 +1,17 @@
+//
+//  VacancyCell.swift
+//  task2_generic_api_client
+//
+//  Created by Roman Brazhnikov on 27.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class VacancyCell: UITableViewCell {
+    
+    @IBOutlet var employerLogoImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+}
---
+++ task2_generic_api_client/VacancyList/VacancyListVC.swift
@@ -0,0 +1,66 @@
+//
+//  VacancyListVC.swift
+//  task2_generic_api_client
+//
+//  Created by Roman Brazhnikov on 27.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class VacancyListVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var vacancyArray = [Vacancy]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    //
+    //  LIFECYCLE
+    //
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let request = InMemoryRequest(filter: "iOS", field: "title")
+        request.execute {
+            (result) in
+            
+            switch(result) {
+            case let .success(resultArray):
+                self.vacancyArray = resultArray
+            case let .error(error):
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    //
+    //  DataSource protocol
+    //
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vacancyArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // dequeuing reusable cell view
+        let cell: VacancyCell = tableView.dequeueReusableCell(withIdentifier: "StandardCell", for: indexPath) as! VacancyCell
+        
+        // getting necessary item
+        let item = vacancyArray[indexPath.row]
+        
+        // populating the cell with data from the item
+        cell.titleLabel?.text = item.title
+        cell.descriptionLabel?.text = item.description
+        
+        return cell
+    }
+    
+    //
+    // LOGIC
+    //
+}
